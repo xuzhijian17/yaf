d3a56d5254d00d7180167f15995cf1ef94dfb733
xuzhijian17 <xuzhijian17@gmail.com>
2020-05-15 20:31:45 +0800

    first commit

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -2,6 +2,7 @@
 class IndexController extends Yaf_Controller_Abstract {
    /* default action */
    public function indexAction() {
+	   var_dump($this->request);
        $this->_view->word = "hello world";
        //or
        // $this->getView()->word = "hello world";
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-define("APPLICATION_PATH",  dirname(__FILE__));
+define("APPLICATION_PATH",  dirname(dirname(__FILE__)));
 
 $app  = new Yaf_Application(APPLICATION_PATH . "/conf/application.ini");
 $app->bootstrap() //call bootstrap methods defined in Bootstrap.php
